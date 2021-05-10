41386a6042e3b1a019723f649f3782a69723e0dc
user <user@MacBook-Pro-de-Visnya-Yutronic.local>
2021-05-09 23:57:31 -0400

    ultimo commit

--- Lab4.js
+++ Lab4.js
@@ -3,17 +3,39 @@ let prompt = require('prompt-sync')({sigint:true});
 
 function view(counter)
 {
-return 'Count: ' + counter + '\n\n (-) (+) \n\n (q) for quit'
+    return '    Count: ' + counter + '\n\n    (-) (+) \n\n\n    (q) for quit \n';
 }
 
 function update(msg,counter)
 {
-
+    if(msg === '-')
+    {
+        return counter - 1;
+    }
+    else if(msg === '+')
+    {
+        return counter + 1;
+    }
+    else
+    {
+        return counter;
+    }
 }
 
 function app(counter)
 {
-
+    while(true)
+    {
+        const currentView = view(counter);
+        console.clear();
+        console.log(currentView);
+        const msg = prompt('What would you do? ');
+        counter = update(msg,counter);
+        if(msg === 'q')
+        {
+            break;
+        }
+    }
 }
 
 app(0)
